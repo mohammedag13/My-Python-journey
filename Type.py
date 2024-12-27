aec6a5391ef8b7102b23983fe0b69cb9769a23f7
mohammedag13 <mohammed.agoumil@gmail.com>
2024-12-27 17:39:01 +0000

    Truncating and rounding

---
+++ Type.py
@@ -0,0 +1,12 @@
+"""
+This code explains truncating and rounding a number,
+with a decimal point
+"""
+#Importing math library
+from math import trunc
+x = 5.5153
+Truncated_x = trunc(x)
+rounded_x = round(x)
+
+print('This is the truncated and rounded result of x: ')
+print(f"This is the truncated result: {Truncated_x}, this is the rounded result {rounded_x}")
