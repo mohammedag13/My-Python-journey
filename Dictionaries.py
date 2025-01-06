0bfeb454e049fe265d5d70f28bd50c8a62542c0c
mohammedag13 <mohammed.agoumil@gmail.com>
2025-01-06 00:58:19 +0000

    dictionaries

---
+++ Dictionaries.py
@@ -0,0 +1,19 @@
+students = {
+    "MOAG": "MOHAMMED AGOUMIL",
+    "HAAG": "HAMZA AGOUMIL",
+    "FAAG": "FATIMA EZZAHRA AGOUMIL",
+}
+
+# Accessing a value
+print(students["MOAG"])
+
+# Adding a new key-value pair
+students["FAAN"] = "FATNA ANIBI"
+print(students) 
+
+# Removing a key-value pair using del
+del students["MOAG"]
+print(students)
+
+for student, lastname in students.items():
+    print(f"{student} last name is {lastname}")
